a1333e1d6a206eaab834665b25644d8be1654875
Gabriel Vallés <hernandezvallesgabriel@gmail.com>
2024-12-28 18:34:23 -0400

    feat: ice servers are now not generated on every request

--- src/app/api/ice/route.ts
+++ src/app/api/ice/route.ts
@@ -1,20 +1,5 @@
 export const GET = async () => {
-  const secretKey = process.env.METERED_SECRET_KEY as string;
-
-  const res = await fetch(
-    `https://lite-builder.metered.live/api/v1/turn/credential?secretKey=${secretKey}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        expiryInSeconds: 7200 * 2, // 2 hours
-      }),
-    },
-  );
-
-  const { apiKey } = await res.json();
+  const apiKey = process.env.METERED_API_KEY as string;
 
   const response = await fetch(
     `https://lite-builder.metered.live/api/v1/turn/credentials?apiKey=${apiKey}`,
